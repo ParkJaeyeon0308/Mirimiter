70fcfdc44a385016f36bb79ffc3458d9df5619af
ParkJaeyeon0308 <wowoyeoni@gmail.com>
2021-04-24 00:02:11 +0900

    로그인, 회원가입 반응형 98%, launch sdk 에러 수정중

--- app/src/main/java/com/example/mirimiter/SignupActivity.java
+++ app/src/main/java/com/example/mirimiter/SignupActivity.java
@@ -18,7 +18,7 @@ public class SignupActivity extends AppCompatActivity {
         SignUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
