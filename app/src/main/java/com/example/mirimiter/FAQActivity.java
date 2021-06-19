6015de32da12d0c840f91f9b2e77290c92574658
ParkJaeyeon0308 <wowoyeoni@gmail.com>
2021-06-19 21:36:14 +0900

    menu x, cardview로 대체

--- app/src/main/java/com/example/mirimiter/FAQActivity.java
+++ app/src/main/java/com/example/mirimiter/FAQActivity.java
@@ -1,4 +1,3 @@
-
 package com.example.mirimiter;
 
 import android.content.Intent;
@@ -13,16 +12,34 @@ import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class FAQActivity extends AppCompatActivity {
     private ImageView back_btn;
     private EditText inputchat;
+    private CardView chat1;
+    private CardView chat2;
+    private CardView chat3;
+    private CardView chat4;
+    private CardView chat5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        back_btn = (ImageView)findViewById(R.id.back_btn);
+        //화면 시작때 키패드 나오지 않게
+        inputchat = (EditText) findViewById(R.id.inputChat);
+        inputchat.setInputType(0);
+
+        //만들어둔 챗봇 선택지 정의해놨음! 사용하셈
+        chat1 = (CardView) findViewById(R.id.chat1);
+        chat2 = (CardView) findViewById(R.id.chat2);
+        chat3 = (CardView) findViewById(R.id.chat3);
+        chat4 = (CardView) findViewById(R.id.chat4);
+        chat5 = (CardView) findViewById(R.id.chat5);
+
+        //뒤로가기
+        back_btn = (ImageView) findViewById(R.id.back_btn);
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,36 +47,6 @@ public class FAQActivity extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
-        inputchat = (EditText) findViewById(R.id.inputChat);
-        inputchat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupMenu popup = new PopupMenu(FAQActivity.this , inputchat);
-
-                MenuInflater inf = popup.getMenuInflater();
-                inf.inflate(R.menu.chatmenu, popup.getMenu());
-                popup.show();
-            }
-        });
-    }
-    //edittext 클릭 후 메뉴 생성, 실행 한번 해보고 edittext 입력창 숨길지 말지 함 봐줭.
-    //메뉴 디자인은 추후에 내가 수정할게욤.
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.chatmenu, menu);
-        return true;
-    }
-
-    //아이템 선택후 액션 여기서 지정하면 됌!
-    //아이템 수정할 menu xml은 menu폴더 안에 있음 chatmenu
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.itemOne) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
 
